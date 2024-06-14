b709cafed9771551348c9c4a689768ef11453202
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2024-06-14 11:16:40 +0300

    add helper methods for fetching (user album titles, photos and thumbnails of album)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
+require 'net/http'
+
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ show edit update ]
 
   # GET /users or /users.json
   def index
@@ -15,28 +17,8 @@ class UsersController < ApplicationController
   def show
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
   # GET /users/1/edit
   def edit
-  end
-
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
@@ -52,24 +34,54 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def fetch_image_url_of_user(user_id)
+    get("https://picsum.photos/id/#{user_id}/info")['download_url']
   end
+  helper_method :fetch_image_url_of_user
+
+  def fetch_album_titles_of_user(user_id)
+    fetch 'https://jsonplaceholder.typicode.com/albums', 'userId', user_id, 'title'
+  end
+  helper_method :fetch_album_titles_of_user
+
+  def fetch_photo_urls_of_album(album_id)
+    fetch 'https://jsonplaceholder.typicode.com/photos', 'albumId', album_id, 'url'
+  end
+  helper_method :fetch_photo_urls_of_album
+
+  def fetch_photo_thumbnail_urls_of_album(album_id)
+    fetch 'https://jsonplaceholder.typicode.com/photos', 'albumId', album_id, 'thumbnailUrl'
+  end
+  helper_method :fetch_photo_thumbnail_urls_of_album
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:search, :name, :username, :email, :phone, :website, :street, :suite, :city, :zipcode, :lat, :lng, :company_name, :catch_phrase, :bs)
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:search, :name, :username, :email, :phone, :website, :street, :suite, :city, :zipcode, :lat, :lng, :company_name, :catch_phrase, :bs)
+  end
+
+  def get(url)
+    uri = URI.parse url
+    request = Net::HTTP::Get.new uri.to_s
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) { |http|
+      http.request request
+    }
+    JSON.parse response.body
+  end
+
+  def fetch(target_url, lookup_name, lookup_id, resource_name)
+    resources = get(target_url)
+
+    desireds = []
+    resources.each do |resource|
+      next unless resource[lookup_name] == lookup_id
+      desireds << resource[resource_name]
     end
+    desireds
+  end
 end
