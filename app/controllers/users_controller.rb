f8b471ca4ef39e1ac65e0ad2bfae9d5ec5f6d059
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2024-06-17 19:46:53 +0300

    add turbo frame for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,12 +34,10 @@ class UsersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:search, :name, :username, :email, :phone, :website, :street, :suite, :city, :zipcode, :lat, :lng, :company_name, :catch_phrase, :bs)
   end
